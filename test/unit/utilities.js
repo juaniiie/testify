1be6d328f7c75cbb73063378c40306d485b14824
juaniiie <becjuana@gmail.com>
2015-10-01 14:40:29 -0700

    Wrote tests in utilities.js

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -3,12 +3,47 @@ var should = chai.should();
 var expect = chai.expect;
 
 describe('the todo.App', function() {
-  describe('the todo object', function(){
+  describe('the todo object', function() {
 
-    it('should have all the necessary methods', function(){
+    it('should have all the necessary methods', function() {
+      expect(todo.util.trimTodoName).to.exist;
+      expect(todo.util.isValidTodoName).to.exist;
+      todo.util.should.have.property('getUniqueId');
     });
   });
 });
 
 describe('the todo.util methods', function() {
+  it('should have a function trimTodoName', function() {
+    assert.typeOf(todo.util.trimTodoName, 'function', 'is a function');
+  });
+  it('should have a function isValidTodoName', function() {
+    assert.typeOf(todo.util.isValidTodoName, 'function', 'is a function');
+  });
+  it('should have a function getUniqueId', function() {
+    assert.typeOf(todo.util.getUniqueId, 'function', 'is a function');
+  });
+});
+
+describe('trimTodoName method', function() {
+  it('should trim sond get rid of white spaces', function() {
+    todo.util.trimTodoName('laundry  ').should.equal('laundry');
+  });
+  it('should reduce the length of todos that have trailing whitespace', function() {
+    todo.util.trimTodoName(' gym ').should.have.length(3);
+  });
+});
+
+describe('isValidTodoName method', function() {
+  it('should evaluate a the validity of a todo', function() {
+    todo.util.isValidTodoName('o    ').should.not.equal(true);
+  });
+});
+
+describe('getUniqueId method', function() {
+  it('should increse by 1', function() {
+    expect(todo.util.getUniqueId()).to.equal(1);
+    expect(todo.util.getUniqueId()).to.equal(2);
+    expect(todo.util.getUniqueId()).to.equal(3);
+  });
 });
